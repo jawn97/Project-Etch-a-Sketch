6fad92e492888e233b908773931221cae2fb0d0a
Jonathan Manzo Pelayo <135053922+jawn97@users.noreply.github.com>
2024-04-08 13:05:05 -0700

    finished TOP etch a sketch project

---
+++ script.js
@@ -0,0 +1,60 @@
+const container = document.querySelector("#container");
+const resetButton = document.querySelector("button");
+
+function createGrid (numberOfCells) {
+  const wrapper = document.createElement('div');
+  wrapper.classList.add('wrapper');
+
+  for (let i = 0; i < numberOfCells; i++) {
+    const row = document.createElement('div');
+    row.classList.add('grid-row');
+
+    for (let j = 0; j < numberOfCells; j++) {
+      const widthAndHeight = 960 / numberOfCells;
+      const gridBox = document.createElement('div');
+      gridBox.classList.add('grid-box');
+      gridBox.style.width = `${widthAndHeight}px`;
+      gridBox.style.height = `${widthAndHeight}px`;
+      row.appendChild(gridBox);
+
+      // adding mousehover listener to change background color
+      gridBox.addEventListener('mouseover', () => {
+        gridBox.style.backgroundColor = 'black';
+      })
+      
+    }
+    wrapper.appendChild(row);
+  }
+  container.appendChild(wrapper);
+}
+
+resetButton.addEventListener('click', () => {
+  let userSize = Number(prompt('What dimensions do you want for the new grid?'));
+
+  while (userSize > 100) {
+    userSize = Number(prompt('Pick a smaller number than 100'))
+  }
+
+  const wrapper = document.querySelector('.wrapper');
+
+  if (!wrapper) {
+    createGrid(userSize)
+  } else {
+    wrapper.remove()
+    createGrid(userSize)
+  }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
